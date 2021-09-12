53a039cdf7e8119b3ea45d60e3993ce3b94de923
charlie83xt <charlie83xt@hotmail.com>
2021-09-12 15:17:26 +0100

    interactive user-input students list code

--- directory.rb
+++ directory.rb
@@ -1,18 +1,22 @@
 
 # first we put a list of students into an array
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex Delarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # Create empty array
+  students = []
+  name = gets.chomp
+  # While the name is not empty repeat this code
+  while !name.empty? do
+      # add the name to the array
+      students << {name: name, cohorts: :november}
+      puts "Now we have #{students.count} students"
+      # get another name from the user
+      name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
 def print_header
   puts "The student of Villains Academy"
@@ -29,6 +33,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header()
 print(students)
 print_footer(students)
